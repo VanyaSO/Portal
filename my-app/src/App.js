8bec588ca1835c2469bd13ad7cb13cdf390778f9
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-12-22 16:17:25 +0200

    redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
 import './index.css';
 import Modal from "./components/Modal";
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+
 
 function App() {
-  return (
-    <Modal>Text</Modal>
-  );
+
+
+
+
+    return (
+      <>
+            <button onClick={open}> click </button>
+
+            {stateMod1 &&
+                <Modal>Text</Modal>
+            }
+      </>
+    );
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const defaultState = {
+    stateMod1: true,
+}
+
+const reducer = (state= defaultState, action) => {
+    switch (action.type) {
+
+        case "Open__MOD":
+            return {...state, stateMod1: action.payload}
+
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>
 );
 
