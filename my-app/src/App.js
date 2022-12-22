3c8e06083e3e7f84860a268220451201781edd1d
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-12-22 16:17:45 +0200

    redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,12 @@ import {useDispatch, useSelector} from "react-redux";
 
 function App() {
 
+    const dispatch = useDispatch();
+    const stateMod1 = useSelector(state => state.stateMod1);
 
+    const open = () => {
+      dispatch({type:"Open__MOD", payload:!stateMod1})
+    }
 
 
     return (
